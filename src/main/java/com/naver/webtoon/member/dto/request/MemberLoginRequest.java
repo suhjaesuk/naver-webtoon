ed3fe7f2ab06aaf1973e47562617161400d2a06a
suhjaesuk <suhjaesuk@gmail.com>
2023-05-11 20:26:53 +0900

    feat: 회원 도메인 로그인 테스트 코드 작성

--- src/main/java/com/naver/webtoon/member/dto/request/MemberLoginRequest.java
+++ src/main/java/com/naver/webtoon/member/dto/request/MemberLoginRequest.java
@@ -7,4 +7,9 @@ public class MemberLoginRequest {
 
     private String username;
     private String password;
+
+    public MemberLoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
