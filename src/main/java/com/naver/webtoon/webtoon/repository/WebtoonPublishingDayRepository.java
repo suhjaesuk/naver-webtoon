3f31a736e54c1bb172f51bdb803f9728c801a497
suhjaesuk <suhjaesuk@gmail.com>
2023-05-16 20:39:54 +0900

    feat: 웹툰 id로 여러개의 WebtoonPublishingDay를 지우는 함수 작성

--- src/main/java/com/naver/webtoon/webtoon/repository/WebtoonPublishingDayRepository.java
+++ src/main/java/com/naver/webtoon/webtoon/repository/WebtoonPublishingDayRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WebtoonPublishingDayRepository extends JpaRepository<WebtoonPublishingDay, Long> {
 
+    void deleteByWebtoonId(Long webtoonId);
 }
