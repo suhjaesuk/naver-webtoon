811f256bc6b4286f28260d37295959d975a4bd12
suhjaesuk <suhjaesuk@gmail.com>
2023-05-16 20:38:20 +0900

    feat: 이넘 한글에서 영어로 변경

--- src/main/java/com/naver/webtoon/webtoon/entity/enums/DayOfTheWeek.java
+++ src/main/java/com/naver/webtoon/webtoon/entity/enums/DayOfTheWeek.java
@@ -6,24 +6,24 @@ import static com.naver.webtoon.common.exception.ErrorCode.NOT_FOUND_DAY_OF_THE_
 
 public enum DayOfTheWeek {
 
-    월요일,
-    화요일,
-    수요일,
-    목요일,
-    금요일,
-    토요일,
-    일요일,
+    MON,
+    TUES,
+    WED,
+    THU,
+    FRI,
+    SAT,
+    SUN,
     ;
 
     public static DayOfTheWeek toEnum(String dayOfTheWeek) {
         return switch (dayOfTheWeek) {
-            case "월요일" -> 월요일;
-            case "화요일" -> 화요일;
-            case "수요일" -> 수요일;
-            case "목요일" -> 목요일;
-            case "금요일" -> 금요일;
-            case "토요일" -> 토요일;
-            case "일요일" -> 일요일;
+            case "MON" -> MON;
+            case "TUES" -> TUES;
+            case "WED" -> WED;
+            case "THU" -> THU;
+            case "FRI" -> FRI;
+            case "SAT" -> SAT;
+            case "SUN" -> SUN;
             default -> throw new WebtoonException(NOT_FOUND_DAY_OF_THE_WEEK);
         };
     }
--- src/main/java/com/naver/webtoon/webtoon/entity/enums/SerializedStatus.java
+++ src/main/java/com/naver/webtoon/webtoon/entity/enums/SerializedStatus.java
@@ -6,16 +6,16 @@ import static com.naver.webtoon.common.exception.ErrorCode.NOT_FOUND_SERIALIZED_
 
 public enum SerializedStatus {
 
-    연재중,
-    휴재중,
-    완결,
+    SERIALIZED,
+    BREAK,
+    COMPLETE,
     ;
 
     public static SerializedStatus toEnum(String serializedStatus) {
         return switch (serializedStatus) {
-            case "연재중" -> 연재중;
-            case "휴재중" -> 휴재중;
-            case "완결" -> 완결;
+            case "SERIALIZED" -> SERIALIZED;
+            case "BREAK" -> BREAK;
+            case "COMPLETE" -> COMPLETE;
             default -> throw new WebtoonException(NOT_FOUND_SERIALIZED_STATUS);
         };
     }
