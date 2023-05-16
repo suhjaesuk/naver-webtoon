05bb6e1a2b8898982275d83d605a476d5b39038f
suhjaesuk <suhjaesuk@gmail.com>
2023-05-16 20:34:50 +0900

    feat: 수정, 삭제, 조회 기능 추가

--- src/main/java/com/naver/webtoon/webtoon/controller/WebtoonController.java
+++ src/main/java/com/naver/webtoon/webtoon/controller/WebtoonController.java
@@ -2,11 +2,17 @@ package com.naver.webtoon.webtoon.controller;
 
 import com.naver.webtoon.common.response.SuccessMessage;
 import com.naver.webtoon.webtoon.dto.request.WebtoonRegisterRequest;
+import com.naver.webtoon.webtoon.dto.request.WebtoonUpdateRequest;
+import com.naver.webtoon.webtoon.dto.response.WebtoonInfoListResponse;
 import com.naver.webtoon.webtoon.service.WebtoonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +28,23 @@ public class WebtoonController {
     public ResponseEntity<SuccessMessage<Void>> registerWebtoon(@RequestBody WebtoonRegisterRequest request) {
         webtoonService.registerWebtoon(request);
         return new ResponseEntity<>(new SuccessMessage<>("웹툰등록성공",null), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/webtoon/{webtoonId}")
+    public ResponseEntity<SuccessMessage<Void>> updateWebtoon(@PathVariable Long webtoonId, @RequestBody WebtoonUpdateRequest request) {
+        webtoonService.updateWebtoon(webtoonId, request);
+        return new ResponseEntity<>(new SuccessMessage<>("웹툰수정성공",null), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/webtoon/{webtoonId}")
+    public ResponseEntity<SuccessMessage<Void>> deleteWebtoon(@PathVariable Long webtoonId) {
+        webtoonService.deleteWebtoon(webtoonId);
+        return new ResponseEntity<>(new SuccessMessage<>("웹툰삭제성공",null), HttpStatus.OK);
+    }
+
+    @GetMapping("/webtoon/{publishingDay}/popular")
+    public ResponseEntity<SuccessMessage<WebtoonInfoListResponse>> retrievePopularWebtoonByPublishingDay(@PathVariable String publishingDay) {
+        WebtoonInfoListResponse response = webtoonService.retrievePopularWebtoonByPublishingDay(publishingDay);
+        return new ResponseEntity<>(new SuccessMessage<>("요일별인기순웹툰조회성공",response), HttpStatus.OK);
     }
 }
