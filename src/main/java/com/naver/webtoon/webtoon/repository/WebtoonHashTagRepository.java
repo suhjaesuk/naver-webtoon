65684c2b7556c8c2b15d9ef839d3d9078d2e733d
suhjaesuk <suhjaesuk@gmail.com>
2023-05-16 20:39:27 +0900

    feat: 웹툰 id로 여러개의 WebtoonHashTag를 지우는 delete 함수 작성

--- src/main/java/com/naver/webtoon/webtoon/repository/WebtoonHashTagRepository.java
+++ src/main/java/com/naver/webtoon/webtoon/repository/WebtoonHashTagRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WebtoonHashTagRepository extends JpaRepository<WebtoonHashTag, Long> {
 
+    void deleteByWebtoonId(Long webtoonId);
 }
